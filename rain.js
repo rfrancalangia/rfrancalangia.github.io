9d48c4e73a851dbaf5a68e65e9497903b65bbe6c
Robert Francalangia <robertfrancalangia@Kathleens-OLD-iMac.local>
2017-09-08 13:10:20 -0400

    add background rain

--- rain.js
+++ rain.js
@@ -3,12 +3,14 @@ function Droplet(radius, color, x, y, Xvelo, Yvelo){
   this.color = color;
   this.x = x;
   this.y = y;
+  this.oy = y;
   this.Xvelo = Xvelo;
   this.Yvelo = Yvelo;
   this.drop = function(){
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, Math.PI * 2, (Math.PI * 2) / 3);
     ctx.fillStyle = this.color;
+    ctx.lineTo(this.x, this.y - 10);
     ctx.fill();
   }
   this.update = function(){
@@ -19,23 +21,22 @@ function Droplet(radius, color, x, y, Xvelo, Yvelo){
       this.Xvelo *= -1;
     }
     this.y += this.Yvelo;
-    this.x += this.Xvelo;
+    // this.x += this.Xvelo;
     this.drop();
   }
 }
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 var circleArray = [];
-for (var i = 0; i < 3000; i++){
+for (var i = 0; i < 2000; i++){
   var x = Math.random() * canvas.width;
   var y = Math.random() * canvas.height;
-  var r = Math.round(Math.random() * 255) / 2;
-  var v = Math.random() * 6 + 1;
+  var r = Math.round(Math.random() * 125);
+  var v = Math.random() * 4 + 1;
   var b = Math.round(Math.random() * 255);
   var a = Math.random();
-  circleArray.push(new Droplet(7, 'rgba('+r+',0,'+b+','+a+')', x, y, 0, v));
+  circleArray.push(new Droplet(4, 'rgba('+r+',0,'+b+','+a+')', x, y, 0, v));
 }
 
 function animate(){
